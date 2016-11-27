e11d8457fa555be77310cdfa5bc92556474b08dc
Oscar Lopez <oscarlopez@despegar.com>
2016-11-27 20:10:55 -0300

    filter lib and route v2 fix

--- routes/v2/hotels.js
+++ routes/v2/hotels.js
@@ -36,9 +36,7 @@ module.exports = [
           limit: Joi.number().required().min(1).max(100).integer().positive().description('Items per page. Value between 1 and 100'),
           page: Joi.number().required().min(1).integer().description('Page Number. Min 1'),
           sort: Joi.string().description('Sort Options. +ASC -DESC +date -date(ordena por el campo lastModified), +total-price -total-price, +nightly-price -nightly-price'),
-          filter: Joi.string().description("Filter Options: name, template, logo, domains, online. If you don't put anything, by default API retrieves you the reduce version of sale"),
-          reduce: Joi.boolean().description('Reduce version of Hotel')
-
+          filter: Joi.string().description("Filter Options: name, template, logo, domains, online. If you don't put anything, by default API retrieves you the reduce version of sale")
         }
       },
       tags: ['api']
